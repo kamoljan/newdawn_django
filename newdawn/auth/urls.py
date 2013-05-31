4489d1e690658a0012952753b9e31eceefaa1516
Kamol Mavlonov <kamol@701search.com>
2013-05-31 13:33:12 +0800

    OAuth: Have to work on production

--- newdawn/auth/urls.py
+++ newdawn/auth/urls.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.conf.urls.defaults import *
-from auth.views import *
+from django.conf.urls import patterns, url
+from .views import *
 
 urlpatterns = patterns('',
-    (r'^$', auth_index),
-    (r'^login/$', auth_index),
-    (r'^logged/$', auth_logged),
-    (r'^logout/$', auth_logout),
-    (r'^error/$', auth_error),
+	(r'^$', auth_index),
+	(r'^login/$', auth_index),
+	(r'^logged/$', auth_logged),
+	(r'^logout/$', auth_logout),
+	(r'^error/$', auth_error),
 )
+
+# urlpatterns = patterns("",
+# 	url(
+# 		regex=r'^logged/$',
+# 		view=auth_logged,
+# 	),
+# )
--- newdawn/newdawn/settings/local.py
+++ newdawn/newdawn/settings/local.py
@@ -8,7 +8,7 @@ from base import *
 
 ########## DEBUG CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = False
+DEBUG = True
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
 TEMPLATE_DEBUG = DEBUG
