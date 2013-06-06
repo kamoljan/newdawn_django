f1d13f1e1162ebaf7b177b39d98872db100b404c
Kamol Mavlonov <kamol701@gmail.com>
2013-06-06 04:32:45 +0000

    OAuth2: Added Google

--- newdawn/auth/views.py
+++ newdawn/auth/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.conf import settings
+from django.contrib.messages.api import get_messages
 
 from social_auth import __version__ as version
 
@@ -26,7 +26,7 @@ def auth_logged(request):
 
 
 def auth_error(request):
-	error_msg = request.session.pop(settings.SOCIAL_AUTH_ERROR_KEY, None)
+	error_msg = get_messages(request)
 	return render_to_response('auth/auth_error.html', {'version': version, 'error_msg': error_msg}, RequestContext(request))
 
 
