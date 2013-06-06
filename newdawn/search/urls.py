8a73b6dadd595c43e8779979fc3cabaf109dadb8
Kamol Mavlonov <kamol701@gmail.com>
2013-06-06 10:23:29 +0000

    Search: Implemented quesry search

--- newdawn/search/urls.py
+++ newdawn/search/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, url
 from .views import ad_search
 
-urlpatterns = patterns("",
-	url(
-		regex=r'^$',
-		view=ad_search,
-	    name="search"
-	),
+urlpatterns = patterns(
+    "",
+    url(regex=r'^$', view=ad_search, name="search"),
 )
